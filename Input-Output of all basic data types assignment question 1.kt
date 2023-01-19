30d72ab847c81c82ea768d503f62c5e09892a13b
raj sampat <95075859+RSempire1@users.noreply.github.com>
2023-01-19 12:52:42 +0530

    Add files via upload

---
+++ Input-Output of all basic data types assignment question 1.kt
@@ -0,0 +1,13 @@
+fun main(){
+    println("please enter a number:")
+    val a: Float = readLine()!!.toFloat()
+
+    println(a)
+
+    println("please enter a character or string:")
+    val b= readLine()
+    println(b)
+
+     val auth : Boolean = true
+    println(auth)
+}

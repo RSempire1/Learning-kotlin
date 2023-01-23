9b99f58530fd915627379528810c4a43f84f30b5
raj sampat <95075859+RSempire1@users.noreply.github.com>
2023-01-23 23:01:31 +0530

    Add files via upload

---
+++ assignment 1 Question 6.kt
@@ -0,0 +1,23 @@
+fun main(){
+
+
+    println("Please enter radius of the circle:")
+
+    val r = readLine()!!.toFloat()
+
+    val diameter= 2*r
+
+    println("Diameter of the circle is $diameter")
+
+    val circumference = 2*3.14*r
+
+    println("Circumference of the circle is $circumference")
+
+    val area =3.14*r*r
+
+    println("area of the circle is $area")
+
+
+
+
+}

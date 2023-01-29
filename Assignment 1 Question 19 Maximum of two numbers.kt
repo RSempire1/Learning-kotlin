5b6af3a4717e70d39def7fa7012b28c6327caf13
raj sampat <95075859+RSempire1@users.noreply.github.com>
2023-01-29 23:26:31 +0530

    Add files via upload

---
+++ Assignment 1 Question 19 Maximum of two numbers.kt
@@ -0,0 +1,13 @@
+fun main(){
+
+    print("Please enter 1st number:")
+    val a=readLine()!!.toFloat()
+    print("Please enter 2nd number:")
+    val b= readLine()!!.toFloat()
+
+
+    if(a>b) print("First number is bigger")else print("second number is bigger")
+
+
+
+}

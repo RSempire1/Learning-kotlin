de3246a6162b34c4060dbc73774bfa02d242098a
raj sampat <95075859+RSempire1@users.noreply.github.com>
2023-01-31 15:07:20 +0530

    Add files via upload

---
+++ Assignment 1 Question 23 Alphabet or not.kt
@@ -0,0 +1,28 @@
+fun main() {
+
+println("input value:")
+    val a= readLine()
+
+    if(a!= null)
+
+    for (i in a) {
+
+        if ( i in 'a'..'z' || i in 'A'..'Z') {
+
+            println(i)
+
+
+
+
+
+
+            }
+
+        }
+           print("All the alphabets in the given input are mentioned above")
+    }
+
+
+
+
+

ccc62d3707755da4d6a7fa76548dca11967b701d
raj sampat <95075859+RSempire1@users.noreply.github.com>
2023-01-31 16:19:10 +0530

    Add files via upload

---
+++ Assignment 1 Question 31 input any alphabet and check whether it is vowel or consonant.kt
@@ -0,0 +1,14 @@
+fun main(){
+
+
+    print("Please enter a alphabet:")
+    val a =readLine()
+
+    if(a=="a" || a=="e" || a=="i" || a=="o" || a=="u" || a=="A" || a=="E" || a=="I" || a=="O" || a=="U")
+
+        print("Entered alphabet is vowel")
+    else
+        print("Entered alphabet is consonant")
+
+
+}

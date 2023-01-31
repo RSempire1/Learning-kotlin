9923ed4804fc170c9c4516ab2a9857e56e267207
raj sampat <95075859+RSempire1@users.noreply.github.com>
2023-01-31 16:06:31 +0530

    Add files via upload

---
+++ Assignment 1 Question 26 check whether a number is negative, positive or zero.kt
@@ -0,0 +1,15 @@
+fun main(){
+
+    print("Please enter a number:")
+    val b=readLine()!!.toFloat()
+
+    if(b>0)
+        println("Number is positive")
+    else if(b<0)
+        println("Number is negative")
+    else
+        print("Number is 0")
+
+
+
+}

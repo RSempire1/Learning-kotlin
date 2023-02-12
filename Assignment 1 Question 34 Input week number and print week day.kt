ed4738950177b3dcfe93a1b174273848e4f07671
raj sampat <95075859+RSempire1@users.noreply.github.com>
2023-02-12 16:14:56 +0530

    Add files via upload

---
+++ Assignment 1 Question 34 Input week number and print week day.kt
@@ -0,0 +1,35 @@
+fun main() {
+
+    print("Please enter a number from 1 to 7:")
+    val a = readln().toInt()
+
+    if(a==1) {
+        print("Sunday")
+    }
+
+        else if(a==2) {
+        print("Monday")
+    }
+
+            else if(a==3){
+                print("Tuesday")
+            }
+        else if(a==4){
+            print("Wednesday")
+    }
+    else if(a==5){
+        print("Thursday")
+    }
+    else if(a==6){
+        print("friday")
+    }
+    else if(a==7){
+        print("Saturday")
+    }
+    else{
+        print("Invalid input")
+    }
+
+
+
+}
